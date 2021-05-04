043fd537bf77389a2dfb84d13fbb8c61b8a3c6e1
TjeuBao <1451010013bao@ou.edu.vn>
2021-05-04 10:03:51 +0700

    add comment

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Comment < ApplicationRecord
+end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :comment do
+    
+  end
+end
