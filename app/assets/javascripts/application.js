bc4fb17cf7f524ca2e22075bbb82d7b96d800ed3
TjeuBao <1451010013bao@ou.edu.vn>
2021-06-20 22:07:27 +0700

    change asset config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,5 @@
 //= require ckeditor/config
-//= require ckeditor/init
-//= require ckeditor_custom
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("trix")
-require("@rails/actiontext")
